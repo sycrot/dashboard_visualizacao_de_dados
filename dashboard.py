4752c55b3a57bef49a03728e85f0f0ef0c4f65dc
Thiago José Farias <52984475+sycrot@users.noreply.github.com>
2024-10-04 20:29:43 -0300

    feat: new figs

--- dashboard.py
+++ dashboard.py
@@ -49,8 +49,9 @@ st.markdown("""
     """, unsafe_allow_html=True)
 st.title('Dashboard de Imóveis para Aluguel')
 # Mostrar os primeiros registros
-st.write('Visualização dos dados:')
-st.dataframe(df.head())
+# st.write('Visualização dos dados:')
+st.dataframe(df.head(), use_container_width=True)
+st.divider()
 
 
 # Criar as colunas
@@ -61,18 +62,28 @@ city_counts = df['Cidade'].value_counts().reset_index()
 city_counts.columns = ['Cidade', 'Número de imóveis']
 
 # Gráfico de dispersão do valor do aluguel em relação à área
-fig1 = px.scatter(df, x='Área (m²)', y='Valor do Aluguel', color='Cidade',
+fig1 = px.strip(df, x='Área (m²)', y='Valor do Aluguel', color='Cidade',
 title='Valor do Aluguel por Área (m²)', 
 labels={'Área (m²)': 'Área (m²)', 'Valor do Aluguel': 'Valor do Aluguel'},
 template='plotly_white')
 col1.plotly_chart(fig1)
 
-cols = col2.columns(len(city_counts))
-for i, row in city_counts.iterrows():
-    cols[i].metric(label=f"Cidade {str(row['Cidade'])}", value=f"{row['Número de imóveis']} imóveis")
+#cols = col2.columns(len(city_counts))
+#for i, row in city_counts.iterrows():
+    #cols[i].metric(label=f"Cidade {str(row['Cidade'])}", value=f"{row['Número de imóveis']} imóveis")
+
+# Porcentagem de imoveis que aceitam ou nao animais
+accept_animals = df['Aceita Animais'].value_counts()
+df_animais = pd.DataFrame({
+    'Aceita animais': ['Aceita', 'Não aceita'],
+    'Quantidade': [accept_animals.get(1,0), accept_animals.get(0,0)]
+    })
+fig_animals = px.pie(df_animais, values='Quantidade', names='Aceita animais', title='Porcentagem de Imóveis que aceitam animais',
+hole=0.6, template='plotly_white'
+)
+col2.plotly_chart(fig_animals)
 
 # Gráfico de barras do número de imóveis por cidade
-
 fig2 = px.bar(city_counts, x='Cidade', y='Número de imóveis',
 title='Número de Imóveis por Cidade',
 labels={'Cidade': 'Cidade', 'Número de Imóveis': 'Número de Imóveis'},
@@ -80,10 +91,22 @@ color='Cidade')
 col3.plotly_chart(fig2)
 
 # Gráfico de pizza para distribuição de imóveis mobiliados
-fig3 = px.pie(df, names='Mobiliado', title='Distribuição de Imóveis Mobiliados')
+furnished = df['Mobiliado'].value_counts()
+df_furnished = pd.DataFrame({
+    'Mobiliado': ['Mobiliado', 'Não mobiliado'],
+    'Quantidade': [furnished.get(1,0), furnished.get(0,0)]
+    })
+fig3 = px.pie(df_furnished, values='Quantidade', names='Mobiliado', title='Distribuição de Imóveis Mobiliados')
 col4.plotly_chart(fig3)
 
 # Gráfico de violino do valor do aluguel por número de quartos
 fig4 = px.violin(df, x='Quartos', y='Valor do Aluguel', box=True, points='all',
 title='Distribuição do Valor do Aluguel por Quartos')
 col5.plotly_chart(fig4)
+
+# 
+fig5 = px.scatter(df, x='Área (m²)', y='IPTU', 
+title='Valor do IPTU vs Área do Imóvel',
+labels={'Área (m²)': 'Área (m²)', 'IPTU': 'Valor do IPTU (R$)'},
+template='plotly_white')
+col6.plotly_chart(fig5)
